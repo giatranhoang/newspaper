7cfa83aae64e4fcb1f461991c2b31d87c7aa2322
Gia Tran <tranhoanggia@gmail.com>
2021-08-22 20:58:45 +0700

    Add ntlk

--- newspapermonitor.py
+++ newspapermonitor.py
@@ -7,6 +7,7 @@ import pandas as pd
 from newspaper import Article
 from newspaper import news_pool
 import nltk
+nltk.download('punkt')
 
 def translate_text(target, text):
     """Translates text into the target language.
